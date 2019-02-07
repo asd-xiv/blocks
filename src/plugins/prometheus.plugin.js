12130aa26220482557f39975fa1d855cf13d097e
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-02-07 17:41:45 +0100

    Rework prometheus metrics

--- src/plugins/prometheus.plugin.js
+++ src/plugins/prometheus.plugin.js
@@ -8,27 +8,24 @@ module.exports = {
     if (Config.get("METRICS_WITH_DEFAULT") === true) {
       Prometheus.collectDefaultMetrics()
     }
-
-    const RequestCounter = new Prometheus.Counter({
-      name: `${Config.get("METRICS_NAMESPACE")}__requests_count`,
-      help: "Number of requests",
+    const RequestSummary = new Prometheus.Summary({
+      name: `${Config.get("METRICS_NAMESPACE")}__request_duration_milliseconds`,
+      help: "request duration in milliseconds",
       labelNames: ["method", "route", "status"],
     })
 
     const DurationHistogram = new Prometheus.Histogram({
-      name: `${Config.get("METRICS_NAMESPACE")}__requests_duration`,
-      help: "Duration histogram of http responses (ms)",
+      name: `${Config.get("METRICS_NAMESPACE")}__request_buckets_milliseconds`,
+      help:
+        "request duration buckets in milliseconds",
+      labelNames: ["method", "route", "status"],
       buckets: [20, 40, 60, 100, 160, 260, 420],
     })
 
     return {
       measure: ({ method, route, status, duration }) => {
-        RequestCounter.inc({
-          method,
-          route,
-          status,
-        })
-        DurationHistogram.observe(duration)
+        RequestSummary.labels(method, route, status).observe(duration)
+        DurationHistogram.labels(method, route, status).observe(duration)
       },
 
       getMetrics: () => Prometheus.register.metrics(),
