691043c0e17f0ddd4eecfb7f283075b4ee6a4344
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-11-21 18:39:36 +0100

    fix: Refactor Helmet middleware

--- src/middleware/res-helmet.js
+++ src/middleware/res-helmet.js
@@ -1,35 +1,27 @@
 const debug = require("debug")("Blocks:HelmetMiddleware")
 
 import helmet from "helmet"
+import { get, when, same, pipe, is } from "@mutantlove/m"
+
+const toBool = source => (is(source) ? source === "true" : null)
+
+const getBool = (prop, _default) =>
+  pipe(get(prop), when(is, toBool, same(_default)))(process.env)
 
 module.exports = () => {
-  const CONTENT_SECURITY_POLICY = process.env.HELMET_CONTENT_SECURITY_POLICY
-  const DNS_PREFETCH_CONTROL = process.env.HELMET_DNS_PREFETCH_CONTROL
-  const EXPECT_CT = process.env.HELMET_EXPECT_CT
-  const FEATURE_POLICY = process.env.HELMET_FEATURE_POLICY
-  const FRAMEGUARD = process.env.HELMET_FRAMEGUARD
-  const HIDE_POWER_BY = process.env.HELMET_HIDE_POWER_BY
-  const HSTS = process.env.HELMET_HSTS
-  const IE_NO_OPEN = process.env.HELMET_IE_NO_OPEN
-  const NO_CACHE = process.env.HELMET_NO_CACHE
-  const NO_SNIFF = process.env.HELMET_NO_SNIFF
-  const CROSSDOMAIN = process.env.HELMET_CROSSDOMAIN
-  const REFERER_POLICY = process.env.HELMET_REFERER_POLICY
-  const XSS_FILTER = process.env.HELMET_XSS_FILTER
-
   return helmet({
-    contentSecurityPolicy: CONTENT_SECURITY_POLICY,
-    dnsPrefetchControl: DNS_PREFETCH_CONTROL,
-    expectCt: EXPECT_CT,
-    featurePolicy: FEATURE_POLICY,
-    frameguard: FRAMEGUARD,
-    hidePoweredBy: HIDE_POWER_BY,
-    hsts: HSTS,
-    ieNoOpen: IE_NO_OPEN,
-    noCache: NO_CACHE,
-    noSniff: NO_SNIFF,
-    permittedCrossDomainPolicies: CROSSDOMAIN,
-    referrerPolicy: REFERER_POLICY,
-    xssFilter: XSS_FILTER,
+    contentSecurityPolicy: false,
+    dnsPrefetchControl: getBool("DNS_PREFETCH_CONTROL", true),
+    expectCt: getBool("EXPECT_CT", false),
+    featurePolicy: getBool("FEATURE_POLICY", false),
+    frameguard: getBool("FRAMEGUARD", true),
+    hidePoweredBy: getBool("HIDE_POWER_BY", false),
+    hsts: getBool("HSTS", true),
+    ieNoOpen: getBool("IE_NO_OPEN", true),
+    noCache: getBool("NO_CACHE", true),
+    noSniff: getBool("NO_SNIFF", true),
+    permittedCrossDomainPolicies: getBool("CROSSDOMAIN", true),
+    referrerPolicy: getBool("REFERER_POLICY", false),
+    xssFilter: getBool("XSS_FILTER", true),
   })
 }
