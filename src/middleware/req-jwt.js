2ee499d13e3e849920dbdc9c37e12716037d0615
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2020-02-23 13:57:43 +0100

    fix: If Authorization header not present, make ctx.jwt destructure friendly by setting it to empty object

--- src/middleware/req-jwt.js
+++ src/middleware/req-jwt.js
@@ -6,9 +6,13 @@ import { is, isEmpty } from "@mutantlove/m"
 import { InputValidationError } from "../errors/input"
 
 module.exports = () =>
+  // only active middleware if JWT_SECRET present
   isEmpty(process.env.JWT_SECRET)
     ? null
     : (req, res, next) => {
+        // safe destructuring in route methods
+        req.ctx.jwt = {}
+
         if (is(req.headers.authorization)) {
           try {
             req.ctx.jwt = jwt.verify(
