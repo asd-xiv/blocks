2031c9ac238bcf810ef991b9db1015c547e11eec
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2020-10-27 17:29:34 +0100

    refactor: Using uncurried version of var functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const debug = require("debug")("blocks:Main")
+const debug = require("debug")("blocks:Index")
 
 const connect = require("connect")
 const path = require("path")
@@ -23,54 +23,71 @@ const block = ({
 } = {}) => {
   process.env.STARTUP_TIME = new Date()
 
-  const ROUTE_PATHS = ["./routes/ping.route.js", ...routes]
-  const PLUGIN_PATHS = [
-    path.resolve(__dirname, "plugins", "router.js"),
-    path.resolve(__dirname, "plugins", "query-parser.js"),
-    ...plugins,
-  ]
-  const MIDDLEWARE_PATHS = [
-    "./middleware/req-bootstrap",
-    "./middleware/req-cors",
-    "./middleware/req-route-exists",
-    // "./middleware/req-jwt",
-    "./middleware/req-query",
-    "./middleware/req-body",
-    ...beforeRoute,
-    "./middleware/res-route",
-    ...afterRoute,
-    "./middleware/res-error",
-    ...afterError,
-    "./middleware/res-goodbye-error",
-    ...beforeSend,
-    "./middleware/res-helmet",
-    "./middleware/res-goodbye",
-  ]
+  return pluginus({
+    source: [
+      path.resolve(__dirname, "plugins", "router.js"),
+      path.resolve(__dirname, "plugins", "query-parser.js"),
+      ...plugins,
+    ],
+  }).then(resolvedPlugins => {
+    /*
+     * Register routes
+     */
+    forEach(
+      item => {
+        const { authenticate, authorize, action, ...rest } =
+          typeof item === "string" ? require(item) : item
 
-  return pluginus({ source: PLUGIN_PATHS }).then(Plugins => {
-    forEach(item => {
-      const { authenticate, authorize, action, ...rest } =
-        typeof item === "string" ? require(item) : item
-
-      Plugins.Router.add({
-        ...rest,
-        authenticate: is(authenticate) ? authenticate(Plugins) : () => false,
-        authorize: is(authorize) ? authorize(Plugins) : () => false,
-        action: action(Plugins),
-      })
-    })(ROUTE_PATHS)
+        resolvedPlugins.Router.add({
+          ...rest,
+          authenticate:
+            typeof authenticate === "function"
+              ? authenticate(resolvedPlugins)
+              : () => false,
+          authorize:
+            typeof authorize === "function"
+              ? authorize(resolvedPlugins)
+              : () => false,
+          action: action(resolvedPlugins),
+        })
+      },
+      ["./routes/ping.route.js", ...routes]
+    )
 
     return [
-      // middleware
-      reduce((acc, item) => {
-        const middle =
-          typeof item === "string" ? require(item)(Plugins) : item(Plugins)
+      /*
+       * Middleware `connect` pipeline
+       */
+      reduce(
+        (acc, item) => {
+          const middle =
+            typeof item === "string"
+              ? require(item)(resolvedPlugins)
+              : item(resolvedPlugins)
 
-        return is(middle) ? acc.use(middle) : acc
-      }, connect())(MIDDLEWARE_PATHS),
+          return is(middle) ? acc.use(middle) : acc
+        },
+        connect(),
+        [
+          "./middleware/req-bootstrap",
+          "./middleware/req-cors",
+          "./middleware/req-route-exists",
+          // "./middleware/req-jwt",
+          "./middleware/req-query",
+          "./middleware/req-body",
+          ...beforeRoute,
+          "./middleware/res-route",
+          ...afterRoute,
+          "./middleware/res-error",
+          ...afterError,
+          "./middleware/res-goodbye-error",
+          ...beforeSend,
+          "./middleware/res-helmet",
+          "./middleware/res-goodbye",
+        ]
+      ),
 
-      // plugins
-      Plugins,
+      resolvedPlugins,
     ]
   })
 }
